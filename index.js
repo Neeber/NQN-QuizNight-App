f19f4a65825651d414a10ec02b2e66d92d9fa3f4
Neeber <ozneeber89@gmail.com>
2021-03-17 12:18:41 +1100

    Creation of folders and html files / Express Server serving answer.html

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Neebers Quiz Night`)
+    //res.send(`Neebers Quiz Night`)
+
+    fs.readFile('./html/answer.html', 'utf8', (err, html) => {
+
+        if (!err) return res.send(html);
+        return res.status(500).send('Something went wrong!');
+
+    });
+
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`App Running on http://localhost:3000`));
