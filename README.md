0b55efa4ab7b5605eacecfa4b3bd8585cca5628c
Neeber <ozneeber89@gmail.com>
2021-03-17 13:52:59 +1100

    Updated File Structure - Added nodemon - Design of Answer Sheet

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # QuizNight-App
+This is an an Application designed to run an online or in-person trivia night.
 
 
 # Client Side
-
-
+Users connect to the Answer Sheet as required.
+It is kept very simple answer boxes and submit and reset.
+Design of form is made with Mobile phones in mind.
 
 # Server Side
+Has the ability to start different quizzes and upload questions and answers from a csv.
+Can lock answer sheets 
+
+## To-Do List
+Bootstrap Doc - https://getbootstrap.com/docs/4.0/getting-started/introduction/
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const port = '3000';
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
 app.get('/', (req, res) => {
     //res.send(`Neebers Quiz Night`)
 
-    fs.readFile('./html/answer.html', 'utf8', (err, html) => {
+    fs.readFile('./views/answer.html', 'utf8', (err, html) => {
 
         if (!err) return res.send(html);
         return res.status(500).send('Something went wrong!');
@@ -15,4 +21,4 @@ app.get('/', (req, res) => {
 
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`App Running on http://localhost:3000`));
+app.listen(port, () => console.log(`App Running on http://localhost:3000`));
